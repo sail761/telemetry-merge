8c01bfbdbba7be60f797db9fcb386f9e46dc8ffd
sail761 <priteshpsail@gmail.com>
2025-07-11 15:20:53 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,10 @@
+import json
+from main import merge_data
+
+def test_merge_data():
+    d1 = json.load(open("data-1.json"))
+    d2 = json.load(open("data-2.json"))
+    expected = json.load(open("data-result.json"))
+
+    assert merge_data(d1, d2) == expected
 
